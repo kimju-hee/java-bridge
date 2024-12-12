0b34061c56361034fbc2c2111de535362f988ea9
kimju-hee <lsj240606@naver.com>
2024-12-12 16:42:58 +0900

    feat: 다리 생성하기 기능 구현

--- src/main/java/bridge/view/controller/BridgeController.java
+++ src/main/java/bridge/view/controller/BridgeController.java
@@ -1,20 +1,33 @@
 package bridge.view.controller;
 
+import bridge.BridgeMaker;
+import bridge.BridgeRandomNumberGenerator;
 import bridge.view.InputView;
 import bridge.view.OutputView;
 import bridge.view.handler.InputHandler;
+import java.util.List;
 
 public class BridgeController {
+    public static int bridgeSize = 0;
+
     OutputView outputView = new OutputView();
     InputView inputView = new InputView();
     InputHandler inputHandler = new InputHandler(inputView);
+    BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
+    BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
 
     public void start() {
         bridgeInputStart();
+        makeBridgeStart();
+
     }
 
     public void bridgeInputStart() {
         outputView.printBridgeSizeInputText();
-        inputHandler.receiveValidatedBridgeSize();
+        bridgeSize = inputHandler.receiveValidatedBridgeSize();
+    }
+
+    public void makeBridgeStart() {
+        bridgeMaker.makeBridge(bridgeSize);
     }
 }
